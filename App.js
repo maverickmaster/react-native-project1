70dd02ced0f6b1d2b790aa6ba24d216400784758
milton <milton@miltons-MacBook.local>
2020-12-01 10:05:00 +0800

    change text hello world

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Milton is here! </Text>
+      <Text>Hello World! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "lightblue",
+    Text:"white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
