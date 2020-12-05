5366955e0b2e8f50a1cd1343c5052775b9c4ea7a
milton <milton@miltons-MacBook.local>
2020-12-05 15:49:47 +0800

    final edit

--- App.js
+++ App.js
@@ -5,9 +5,26 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Milton is here now!</Text>
-      <Text>Hello World! </Text>
-      <StatusBar style="auto" />
+      <View style={styles.fillerContainer}></View>
+
+      <View style={styles.titleContainer}>
+        <Text style={styles.text}>Milton is here now!</Text>
+
+        <Text
+          style={{
+            fontStyle: "italic",
+            fontSize: 68,
+            textAlign: "center",
+            color: "red",
+          }}
+        >
+          Hello World!
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.fillerContainer}></View>
     </View>
   );
 }
@@ -15,15 +32,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightblue",
+    backgroundColor: "#ff0",
     alignItems: "center",
     justifyContent: "center",
   },
 
   text: {
-    color: "white",
-    fontSize: 40,
+    color: "grey",
+    fontSize: 20,
     fontWeight: "bold",
+    textAlign: "center",
   },
 
+  fillerContainer: {
+    padding: 10,
+    backgroundColor: "blue",
+    flex: 0.25,
+    width: "50%",
+  },
+
+  titleContainer: {
+    backgroundColor: "orange",
+    width: "100%",
+    padding: 10,
+    margin: 10,
+    flex: 0.5,
+    justifyContent: "center",
+    borderRadius: 20,
+  },
 });
